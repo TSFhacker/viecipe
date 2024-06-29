c6dc15f35301bbacc8426377e7b8b290a5a749d2
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2024-06-29 14:08:29 +0700

    delete middleware

--- middleware.js
+++
@@ -1,7 +0,0 @@
-import { getSession } from "next-auth/react";
-import { NextResponse } from "next/server";
-
-// This function can be marked `async` if using `await` inside
-export async function middleware(request) {}
-
-export const config = {};
